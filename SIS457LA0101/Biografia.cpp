81e332ae56fb138115b6db36af2e93ae831f82f8
Alejandra Terceros Arcani <alita4774@gmail.com>
2022-10-03 01:55:38 -0400

    Actualizacion

--- SIS457LA0101/Biografia.cpp
+++ SIS457LA0101/Biografia.cpp
@@ -2,8 +2,19 @@
 #include <string>
 using namespace std;
 int main() {
+	string x;
 	cout << "Mi nombre es Alejandra Terceros tengo 23 años soy nacida en Sucre pero vivi en La Paz toda mi primaria y secundaria." << endl;
 	cout << "actualmente vivo con mi hermano y estudio en un instituto por las noches llamado ITSEC SUCRE" << endl;
 	cout << "Gracias por leer mi Biografia :D" << endl;
+	cout << "Quiere ver otra vez? escriba SI/NO "<<endl;
+	cin >> x;
+	while (x == "SI")
+	{
+		cout << "Mi nombre es Alejandra Terceros tengo 23 años soy nacida en Sucre pero vivi en La Paz toda mi primaria y secundaria." << endl;
+		cout << "actualmente vivo con mi hermano y estudio en un instituto por las noches llamado ITSEC SUCRE" << endl;
+		cout << "Gracias por leer mi Biografia :D" << endl;
+		cin >> x;
+	}
+	system("pause");
 	return 0;
 }
